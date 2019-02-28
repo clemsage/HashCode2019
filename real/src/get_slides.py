995aa4ad33da4be600b1db1d63c30ad1533401a7
TonioGarcia07 <a.garcia.walecha@gmail.com>
2019-02-28 20:26:17 +0000

    find best vertical combi

--- real/src/get_slides.py
+++ real/src/get_slides.py
@@ -10,6 +10,17 @@ class Slide():
         self.photo_ids = photo_ids
         self.tags = set(tags)
 
+def get_common_tags(photo1, photo2):
+    common_tags = []
+    for tag in photo1.tags:
+        common_tags.append(tag)
+    for tag in photo2.tags:
+        if tag not in common_tags:
+            common_tags.append(tag)
+    
+    return common_tags
+            
+
 def get_slides(list_of_photos):
     results = []
     vertical_photos = []
@@ -21,16 +32,20 @@ def get_slides(list_of_photos):
             slide = Slide([photo.photo_id], photo.tags)
             results.append(slide)
 
-
     for i in range(len(vertical_photos) // 2):
-        photo1 = vertical_photos[2 * i]
-        photo2 = vertical_photos[2 * i + 1]
-        common_tags = photo1.tags
-        for tag in photo2.tags:
-            if tag not in common_tags:
-                common_tags.append(tag)
+        vertical_photo = vertical_photos.pop()
+        return_photo = vertical_photos[0]
+        score = len(get_common_tags(vertical_photo, return_photo))
+        for i_test, test_photo in enumerate(vertical_photos):
+            new_score = len(get_common_tags(vertical_photo, return_photo))
+            if new_score > score:
+                score = new_score
+                return_photo = test_photo
         
-        photo_ids = [photo1.photo_id, photo2.photo_id]
+        best_score_photo = vertical_photos.pop(i_test)
+
+        photo_ids = [vertical_photo.photo_id, best_score_photo.photo_id]
+        common_tags = get_common_tags(vertical_photo, best_score_photo)
         slide = Slide(photo_ids, common_tags)
         results.append(slide)
 
@@ -45,7 +60,10 @@ if __name__ == "__main__":
             self.photo_id = photo_id
 
     list_of_photos = [
-        Photo('H', ['A', 'B', 'C'], 1),
-        Photo('V', ['A', 'B', 'C'], 2),
-        Photo('V', ['D', 'C'], 3),
+        Photo('V', ['A', 'B', 'C'], 1),
+        Photo('V', ['E', 'B', 'C'], 2),
+        Photo('H', ['A', 'B', 'C'], 3),
+        Photo('V', ['D', 'U'], 4),
+        Photo('V', ['R', 'T', 'E'], 5),
     ]
+    slides = get_slides(list_of_photos)
